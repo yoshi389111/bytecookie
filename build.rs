2868b0231b0f85503635ba295c9c567daa64c5a1
sato yoshiyuki <yoshi.389111@gmail.com>
2025-07-22 21:43:47 +0900

    :recycle: refact: Cleaning up dependencies and adding command line options

--- build.rs
+++ build.rs
@@ -1,18 +1,21 @@
-use flate2::Compression;
-use flate2::write::GzEncoder;
-use std::{env, fs, io::Write, path::Path};
+use std::io::Write;
+
+/// embedded JSON file path
+const JSON_PATH: &str = "assets/bytecookies.json";
 
 fn main() {
+    println!("cargo:rerun-if-changed={JSON_PATH}");
+
     // read the JSON file, and minify it.
-    let json_raw = fs::read_to_string("assets/bytecookies.json").expect("Failed to read JSON file");
+    let json_raw = std::fs::read_to_string(JSON_PATH).expect("Failed to read JSON file");
     let json_value: serde_json::Value = serde_json::from_str(&json_raw).expect("Invalid JSON");
     let minified = serde_json::to_string(&json_value).expect("Failed to minify JSON");
 
     // write the minified JSON to a gzipped file in the OUT_DIR
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("bytecookies.json.gz");
-    let file = fs::File::create(&dest_path).expect("Failed to create output file");
-    let mut encoder = GzEncoder::new(file, Compression::default());
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let dest_path = std::path::Path::new(&out_dir).join("bytecookies.json.gz");
+    let file = std::fs::File::create(&dest_path).expect("Failed to create output file");
+    let mut encoder = flate2::write::GzEncoder::new(file, flate2::Compression::default());
     encoder
         .write_all(minified.as_bytes())
         .expect("Failed to write gzip");
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use clap::Parser;
-use serde::Deserialize;
 use std::io::Read;
 
-const BYTE_COOKIES_GZ: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/bytecookies.json.gz"));
+/// Command line arguments
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Args {
+    /// Print your fortune message for today
+    #[arg(short, long, default_value = None)]
+    user: Option<String>,
 
-#[derive(Debug, Deserialize)]
+    /// Message JSON file path
+    #[arg(short, long, env = "BYTE_COOKIES_JSON", hide_env_values = true, default_value = None)]
+    json: Option<String>,
+}
+
+/// Structure for one message in the JSON
+#[derive(Debug, serde::Deserialize)]
 struct ByteCookie {
     snippet: String,
     message: String,
 }
 
+/// Default embedded message JSON (gzip compressed)
+const BYTE_COOKIES_GZ: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/bytecookies.json.gz"));
+
+/// Parse and get embedded JSON
 fn get_embedded_cookies() -> Vec<ByteCookie> {
     let mut json = String::new();
     let mut decoder = flate2::read::GzDecoder::new(BYTE_COOKIES_GZ);
@@ -19,38 +34,44 @@ fn get_embedded_cookies() -> Vec<ByteCookie> {
     serde_json::from_str(&json).expect("JSON parsing failed")
 }
 
+/// Read message JSON from specified file
+fn get_cookies_from_file(file_path: &str) -> Vec<ByteCookie> {
+    let mut file = std::fs::File::open(file_path).expect("Failed to open file");
+    let mut json = String::new();
+    file.read_to_string(&mut json).expect("Failed to read file");
+    serde_json::from_str(&json).expect("JSON parsing failed")
+}
+
+/// Decide random index for message
 fn decide_random_index(count: usize) -> usize {
     let mut rng = rand::rng();
     rand::Rng::random_range(&mut rng, 0..count)
 }
 
-fn decide_todays_index(count: usize, date: &str, user: &str) -> usize {
+/// Decide message index from today's date and user name
+fn decide_todays_index(count: usize, today: &str, user: &str) -> usize {
     let mut context = md5::Context::new();
     context.consume(user.as_bytes());
     context.consume(b":");
-    context.consume(date.as_bytes());
+    context.consume(today.as_bytes());
     let digest = context.finalize();
     let num = u32::from_be_bytes(digest.0[0..4].try_into().unwrap());
     (num as usize) % count
 }
 
-#[derive(Debug, Parser)]
-#[command(version, about)]
-struct Args {
-    /// Print your message today
-    #[arg(short, long, default_value_t = false)]
-    today: bool,
-}
-
+/// Show fortune message for engineers
 fn main() {
     let args = Args::parse();
 
-    let byte_cookies = get_embedded_cookies();
+    let byte_cookies = if let Some(file_path) = args.json {
+        get_cookies_from_file(&file_path)
+    } else {
+        get_embedded_cookies()
+    };
 
-    let index = if args.today {
+    let index = if let Some(user) = args.user {
         let today = chrono::Local::now().format("%Y-%m-%d").to_string();
-        let username = whoami::username();
-        decide_todays_index(byte_cookies.len(), &today, &username)
+        decide_todays_index(byte_cookies.len(), &today, &user)
     } else {
         decide_random_index(byte_cookies.len())
     };
